61371c952bc885a0aec8ad585a543662b1fb063a
Douglas Hall <dhall@edx.org>
2019-01-15 10:42:51 -0500

    Fix build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,37 @@
+const Merge = require('webpack-merge');
 var path = require('path');
 var commonConfig = require('./webpack-common.config.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
+module.exports = Merge.smart(commonConfig, {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    library: 'frontend-component-header',
+    library: 'frontend-component-site-header',
     libraryTarget: 'umd',
   },
-  module: commonConfig.module,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
+  },
   plugins: [
     // Cleans the dist directory before each build
     new CleanWebpackPlugin(['dist'], {
@@ -21,4 +42,4 @@ module.exports = {
       { from: './src/scss', to: './scss' },
     ]),
   ],
-};
+});
