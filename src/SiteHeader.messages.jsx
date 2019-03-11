b774f9c70705fa6a7eb28f28fd3a51fb27c4e652
Adam Butterworth <adamjbutterworth@gmail.com>
2019-03-11 15:44:47 -0400

    fix: add 18n (#10)
    * fix: add i18n
    
    * fix: rename messages file to match convention

---
+++ src/SiteHeader.messages.jsx
@@ -0,0 +1,41 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  'header.label.account.nav': {
+    id: 'header.label.account.nav',
+    defaultMessage: 'Account',
+    description: 'The aria label for the account menu nav',
+  },
+  'header.label.account.menu': {
+    id: 'header.label.account.menu',
+    defaultMessage: 'Account Menu',
+    description: 'The aria label for the account menu trigger',
+  },
+  'header.label.account.menu.for': {
+    id: 'header.label.account.menu',
+    defaultMessage: 'Account menu for {username}',
+    description: 'The aria label for the account menu trigger when the username is displayed in it',
+  },
+  'header.label.main.nav': {
+    id: 'header.label.main.nav',
+    defaultMessage: 'Main',
+    description: 'The aria label for the main menu nav',
+  },
+  'header.label.main.menu': {
+    id: 'header.label.main.menu',
+    defaultMessage: 'Main Menu',
+    description: 'The aria label for the main menu trigger',
+  },
+  'header.label.main.header': {
+    id: 'header.label.main.header',
+    defaultMessage: 'Main',
+    description: 'The aria label for the main header',
+  },
+  'header.label.secondary.nav': {
+    id: 'header.label.main.nav',
+    defaultMessage: 'Secondary',
+    description: 'The aria label for the seconary nav',
+  },
+});
+
+export default messages;
